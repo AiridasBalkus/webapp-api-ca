819958cae343b844c20045d9131fa7664f1464b9
Airidas Balkus <147723109+AiridasBalkus@users.noreply.github.com>
2025-12-20 13:19:05 +0000

    Added users-api.jsx for the user registration process and login

---
+++ react-movies/src/api/users-api.jsx
@@ -0,0 +1,31 @@
+const API = "http://localhost:8080/api/users";
+
+//signup user(regiser)
+
+export const signup = async(username, password) => {
+    const res = await fetch(`${API}?action=register`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+    });
+    const data = await res.json().catch(() => ({}));
+    if (!res.ok) {
+        throw new Error(data.message || "Signup failed try again!");
+    }
+    return data;
+};
+//login user(authenticate)
+export const login = async (username, password) => {
+    const res = await fetch(`${API}?action=login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+    });
+    const data = await res.json().catch(() => ({}));
+    if (!res.ok) {
+        throw new Error(data.message || "Login failed try again!");
+    }
+    return data;
+};
